5471dbaced93eadfb0082efa6654e20a11fbec83
Jaiakash <akashjaiakash555@gmail.com>
2026-08-12 22:30:20 +0530

    seperate database connection

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-import mysql.connector
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from database import get_connection
 
 # Connect to MySQL
-connection = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
-
+connection = get_connection()
 cursor = connection.cursor()
 
 # Get student name
